33f91511cfdff8cedba828de9f47d46155611874
Krutika266 <125389413+Krutika266@users.noreply.github.com>
2024-03-23 18:04:54 +0530

    Add files via upload

---
+++ Ques1.cs
@@ -0,0 +1,37 @@
+using System;
+
+class Ques1
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the height of the triangle:");
+        int height;
+        while (!int.TryParse(Console.ReadLine(), out height) || height < 1)
+        {
+            Console.WriteLine("The height of the traingle must be greater than 0");
+        }
+
+        PrintTriangle(height);
+
+        Console.ReadLine(); 
+    }
+
+    static void printTriangle(int height)
+    {
+        for (int i = 1; i <= height; i++)
+        {
+            
+            for (int j = 0; j < height - i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int k = 0; k < 2 * i - 1; k++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
